9880c61f733e3a82a3922b29ba5e287a54bd57a6
Gustavo Alatta <galatta2@ccc.edu>
2025-01-15 12:39:47 -0600

    #version 3

--- MyFirstProgram/src/myfirstprogram/MyFirstProgram.java
+++ MyFirstProgram/src/myfirstprogram/MyFirstProgram.java
@@ -1,8 +1,16 @@
 package myfirstprogram;
 //Author: Gustavo Alatta
+
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+        
+        int Hours=40;
+        double PayRate=16.5;
+        
+        double Payment=Hours*PayRate;
+        
+        System.out.println("Your Payment is ==> "+Payment);
         
     }
     
